fa6788cb92c20229950cf2d20318c0e5043532c1
foones <foones@gmail.com>
2014-01-06 21:33:59 -0300

    space of cont.funcs. between two dcpo's is a dcpo

--- coq/math1/domain.v
+++ coq/math1/domain.v
@@ -30,11 +30,24 @@ Definition po_least_upper_bound po (S : subset (po_set po)) (lub : po_set po) :=
   po_upper_bound po S lub /\
   forall bound, po_upper_bound po S bound -> lub << bound.
 
+Lemma po_lub_unique : forall po S lub1 lub2,
+                        po_least_upper_bound po S lub1 ->
+                        po_least_upper_bound po S lub2 ->
+                        lub1 = lub2.
+Proof.
+  intros po S lub1 lub2 lub1_prop lub2_prop.
+  apply po_antisym.
+  apply lub1_prop.
+  apply lub2_prop.
+  apply lub2_prop.
+  apply lub1_prop.
+Qed.
+
 Definition po_monotonic A B (f : po_set A -> po_set B) :=
   forall x y, x << y -> f x << f y.
 
-Definition f_ap (A B : Type) (f : A -> B) (S : subset A) :=
-  fun b : B => exists a : A, S a /\ b = f a.
+Definition f_ap (A B : Type) (f : A -> B) (S : subset A) : subset B :=
+  fun b : B => exists a : A, S a /\ f a = b.
 
 Notation "f !! S" := (f_ap _ _ f S) (at level 35).
 
@@ -312,6 +325,15 @@ Proof.
   assumption.
 Qed.
 
+Lemma d_le_extensional :
+  forall D E (f g : D =>> E) x, d_function_le D E f g -> f $ x << g $ x.
+Proof.
+  intros D E f g x H.
+  unfold d_function_le in H.
+  specialize H with x.
+  assumption.
+Qed.
+
 Lemma d_function_le_antisym D E (f g : D =>> E) :
   d_function_le D E f g ->
   d_function_le D E g f ->
@@ -370,57 +392,335 @@ Lemma d_function_domain_bot_continuous D E :
 Proof.
   unfold d_continuous. 
   intros F F_filter lubD lubD_lub lubE lubE_lub.
-
-
-
-Definition d_continuous D E f (f_monotonic : d_monotonic D E f) :=
+  unfold d_function_domain_bot_function.  
+  apply po_lub_unique
+   with (S :=
+         fun x : po_set (d_po E) =>
+           exists y : d_set D,
+             F y /\ d_function_domain_bot_function D E y = x).
+  apply lubE_lub.
+  unfold po_least_upper_bound.
+  split.
+    (* bot is upper bound *)
+    unfold po_upper_bound.
+    intros x x_prop.
+    elim x_prop.
+    intros y x_shape.
+    destruct x_shape as (Fy, dom_bot_func_y_eq_x).
+    rewrite <- dom_bot_func_y_eq_x.
+    unfold d_function_domain_bot_function.
+    apply po_refl.
+    (* bot is least upper bound *)
+    intros.
+    apply d_bot_prop.
+Qed.
 
 Definition d_function_domain_bot D E : po_set (d_function_space_partial_order D E).
-  apply exist with (fun _ : d_set D => d_bot E).
+  exists (d_function_domain_bot_function D E).
+  exists (d_function_domain_bot_monotonic D E).
   apply d_function_domain_bot_continuous.
+Defined.
+  
+Lemma d_function_domain_bot_apply :
+  forall D E x,
+    d_function_domain_bot D E $ x = d_bot E.
+Proof.
+  intros.
+  unfold d_function_ap.
+  unfold d_function_domain_bot.
+  unfold proj1_sig.
+  reflexivity.
+Qed.
+
+Lemma d_function_domain_bot_prop D E :
+  forall f : po_set (d_function_space_partial_order D E),
+    d_function_domain_bot D E << f.
+Proof.
+  intros.
+  simpl.
+  unfold d_function_le.
+  intros.
+  rewrite d_function_domain_bot_apply.
+  apply d_bot_prop.
+Qed.
+
+Lemma d_function_domain_lub_function_filter :
+  forall D E F (F_filter : po_filter (d_function_space_partial_order D E) F) x,
+    po_filter (d_po E) (fun y : d_set E => exists f, F f /\ f $ x = y).
+Proof.
+  intros D E F F_filter x.
+  unfold po_filter.
+  intros y1 y2.
+  intros y1_prop y2_prop.
+  elim y1_prop; intros f1 y1_shape.
+  elim y2_prop; intros f2 y2_shape.
+  destruct y1_shape as (f1_in_F, f1_x_eq_y1).
+  destruct y2_shape as (f2_in_F, f2_x_eq_y2).
+  assert (exists g, F g /\ f1 << g /\ f2 << g) as exists_g.
+    apply F_filter.
+    assumption.
+    assumption.
+  elim exists_g; intros g g_prop.
+  exists (g $ x).
+  split.
+    exists g.
+    split.
+    apply g_prop.
+    reflexivity.
+    replace y1 with (f1 $ x).
+    replace y2 with (f2 $ x).
+    destruct g_prop as (g_in_F, (f1_le_g, f2_le_g)).
+    
+    split.
+       apply d_le_extensional; assumption.
+       apply d_le_extensional; assumption.
+Qed.
+
+Definition d_function_domain_lub_function 
+           D E F (F_filter : po_filter (d_function_space_partial_order D E) F) :
+           d_set D -> d_set E :=
+  fun x : d_set D =>
+    d_lub E
+          (fun y : d_set E => exists f, F f /\ f $ x = y)
+          (d_function_domain_lub_function_filter D E F F_filter x).
+
+Lemma d_functional_monotonic :
+  forall D E (f : D =>> E), d_monotonic D E (proj1_sig f).
+Proof.
+  intros D E f.
+  case f.
+  simpl.
+  intros f_function f_shape.
+  destruct f_shape.
+  assumption.
+Qed.
+
+Lemma d_functional_continuous :
+  forall D E (f : D =>> E), d_continuous D E
+                                         (proj1_sig f)
+                                         (d_functional_monotonic D E f).
+Proof.
+  intros D E f.
+  case f.
+  simpl.
+  intros f_function f_prop.
+  destruct f_prop.
+  assumption.
+Qed.
+
+Lemma d_function_domain_lub_monotonic
+           D E F (F_filter : po_filter (d_function_space_partial_order D E) F) :
+             d_monotonic D E (d_function_domain_lub_function D E F F_filter).
+Proof.
+  unfold d_monotonic.
+  unfold po_monotonic.
+  intros x y x_le_y.
+  unfold d_function_domain_lub_function.
+  simpl.
+  apply d_lub_prop.
+  unfold po_upper_bound.
+  intros fx fx_prop.
+  elim fx_prop. intros f fx_shape.
+  destruct fx_shape as (f_in_F, f_ap_x_eq_fx).
+  apply po_trans with (f $ y).
+  rewrite <- f_ap_x_eq_fx.
+  assert (d_monotonic D E (proj1_sig f)) as f_monotonic.
+    apply d_functional_monotonic.
+  apply f_monotonic.
+  assumption.
+  apply d_lub_prop.
+  exists f.
+  split.
+  assumption.
+  reflexivity.
+Qed.
+
+Lemma d_functional_preserves_filter :
+  forall D E (f : D =>> E) F, d_filter D F -> d_filter E (proj1_sig f !! F).
+Proof.
+  intros D E f F F_filter.
+  unfold d_filter.
+  unfold po_filter.
+  intros a b a_prop b_prop.
+  unfold f_ap in a_prop.
+  unfold f_ap in b_prop.
+  elim a_prop.
+  intros a_pre a_shape.
+  elim b_prop.
+  intros b_pre b_shape.
+  destruct a_shape as (a_pre_in_F, a_eq_f_a_pre).
+  destruct b_shape as (b_pre_in_F, b_eq_f_b_pre).
+  assert (exists c_pre, F c_pre /\ a_pre << c_pre /\ b_pre << c_pre)
+      as exists_c_pre.
+    apply F_filter.
+    assumption.
+    assumption.
+  elim exists_c_pre.
+  intros c_pre c_pre_shape.
+  destruct c_pre_shape as (c_pre_in_F, (a_pre_le_c_pre, b_pre_le_c_pre)).
+  exists (proj1_sig f c_pre).
+  assert (d_monotonic D E (proj1_sig f)) as f_monotonic.
+    apply d_functional_monotonic.
+  split.
+    unfold f_ap.
+    exists c_pre. 
+    split. assumption. reflexivity.
+    split.
+    rewrite <- a_eq_f_a_pre. 
+    apply f_monotonic.
+    assumption.
+    rewrite <- b_eq_f_b_pre. 
+    apply f_monotonic.
+    assumption.
+Qed.
+
+Lemma d_function_domain_lub_continuous
+           D E F (F_filter : po_filter (d_function_space_partial_order D E) F) :
+             d_continuous D E
+                          (d_function_domain_lub_function D E F F_filter)
+                          (d_function_domain_lub_monotonic D E F F_filter).
+Proof.
+  unfold d_continuous.
+  intros G G_filter lubD lubD_lub lubE lubE_lub.
+  apply po_lub_unique
+   with (S := fun x : d_set E =>
+                exists y : d_set D,
+                  G y /\ d_function_domain_lub_function D E F F_filter y = x).
+  assumption.
+  unfold po_least_upper_bound.
+  split.
+  (* upper bound *)
+  unfold po_upper_bound.
+  intros lub_fx lub_fx_prop.
+  elim lub_fx_prop.
+  intros x lub_fx_shape.
+  destruct lub_fx_shape as (x_in_G, lub_f_ap_x_eq_lub_fx).
+  rewrite <- lub_f_ap_x_eq_lub_fx.
+  apply d_lub_prop.
+  unfold po_upper_bound.
+  intros f_ap_x f_ap_x_prop.
+  elim f_ap_x_prop.
+  intros f f_ap_x_shape.
+  destruct f_ap_x_shape as (f_in_F, f_x_eq_f_ap_x).
+  rewrite <- f_x_eq_f_ap_x.
+  apply po_trans with (f $ lubD).
+  assert (d_monotonic D E (proj1_sig f)) as f_monotonic.
+    apply d_functional_monotonic.
+  apply f_monotonic.
+  apply lubD_lub.
+  assumption.
+  apply d_lub_prop.
+  exists f.
+  split.
+  assumption.
+  reflexivity.
+  (* least *)
+  intros z z_prop.
+  apply d_lub_prop.
+  unfold po_upper_bound.
+  intros flubD flubD_prop.
+  elim flubD_prop.
+  intros f f_shape.
+  destruct f_shape as (f_in_F, f_lubD_eq_flubD).
+  rewrite <- f_lubD_eq_flubD.
+  (* using the fact that f is continuous *)
+  assert (d_continuous D E (proj1_sig f) (d_functional_monotonic D E f))
+      as f_continuous.
+    apply d_functional_continuous.
+  unfold d_continuous in f_continuous.
+  unfold d_function_ap.
+  rewrite <- f_continuous
+     with (F := G)
+          (lubE := d_lub E
+                     (proj1_sig f !! G)
+                     (d_functional_preserves_filter D E f G G_filter)).
+    (* (1) *)
+    apply d_lub_prop.
+      unfold po_upper_bound.
+      intros fx fx_in_fG.
+      unfold f_ap in fx_in_fG.
+      elim fx_in_fG.
+      intros x fx_shape.
+      destruct fx_shape as (x_in_G, fx_eq_f_x).
+      rewrite <- fx_eq_f_x.
+      apply po_trans
+       with (d_lub E
+                   (fun y => exists f, F f /\ f $ x = y)
+                   (d_function_domain_lub_function_filter D E F F_filter x)).
+      (* trans1 *)
+        apply d_lub_prop.
+        exists f.
+        split.
+        assumption.
+        unfold d_function_ap.
+        reflexivity.
+      (* trans2 *)
+        apply z_prop.
+        exists x. 
+        split.
+        assumption.
+        unfold d_function_domain_lub_function.        
+        reflexivity.
+    (* (2) *)
+    assumption.
+    (* (3) *)
+    assumption.
+    (* (4) *)
+    unfold f_ap.
+    apply d_lub_prop.
+Qed.
+
+Definition d_function_domain_lub
+             D E F (F_filter : po_filter (d_function_space_partial_order D E) F) :
+             po_set (d_function_space_partial_order D E).
+  exists (d_function_domain_lub_function D E F F_filter).
+  exists (d_function_domain_lub_monotonic D E F F_filter).
+  apply (d_function_domain_lub_continuous D E F F_filter).
+Defined.
+
+Definition d_function_domain_lub_prop
+             D E F (F_filter : po_filter (d_function_space_partial_order D E) F) :
+             po_least_upper_bound (d_function_space_partial_order D E)
+                                  F (d_function_domain_lub D E F F_filter).
+  unfold po_least_upper_bound.
+  split.
+  (* upper *)
+    unfold po_upper_bound.
+    intros f f_in_F.
+    simpl.
+    unfold d_function_le.
+    intro.
+    simpl.
+    unfold d_function_domain_lub_function.
+    apply d_lub_prop.
+    exists f. 
+    split.
+    assumption.
+    reflexivity.
+  (* least *)
+    intros g g_upper_bound.
+    simpl. 
+    unfold d_function_le.
+    intro x.
+    simpl.
+    unfold d_function_domain_lub_function.
+    apply d_lub_prop.
+    unfold po_upper_bound.
+    intro fx.
+    intro fx_prop.
+    elim fx_prop.
+    intros f fx_shape.
+    destruct fx_shape as (f_in_F, f_x_eq_fx).
+    rewrite <- f_x_eq_fx.
+    apply d_le_extensional.
+    apply g_upper_bound.
+    assumption.
+Defined.
  
 Definition d_function_domain (D E : domain) :=
   mk_domain
-    (d_function_space_partial_order D E).
-
-
-
-(** Examples **)
- 
-Definition poset_nat :=
-  mk_partial_order
-    nat
-    le
-    le_refl
-    le_antisym
-    le_trans.
-
-Definition s123 : subset nat :=
-  fun x =>
-    x = 1 \/ x = 2 \/ x = 3.
-
-Lemma lub_s123 : least_upper_bound poset_nat s123 3.
-Proof.
-  unfold least_upper_bound.
-  split.
-  (* upper bound *)
-    unfold po_upper_bound.
-    intros x x_cases.
-    unfold s123 in x_cases.
-    case x_cases.
-      intro; compute; replace x with 1; auto.
-    intro x_cases2.
-    case x_cases2.
-      intro; compute; replace x with 2; auto.
-      intro; compute; replace x with 3; auto.
-  (* least *)
-    intros b b_upper_bound.
-    compute.
-    unfold po_upper_bound in b_upper_bound.
-    compute in b_upper_bound.
-    specialize b_upper_bound with 3.
-    apply b_upper_bound.
-    right. right.
-    reflexivity.
-Qed.
-
+    (d_function_space_partial_order D E)
+    (d_function_domain_bot D E)
+    (d_function_domain_bot_prop D E)
+    (d_function_domain_lub D E)
+    (d_function_domain_lub_prop D E).
